4e9eb8df3fe690e45a0478409e0e6a0a544c3832
Gilmar Shuenck <gilmar_shuenck@hotmail.com>
2024-11-19 14:54:33 -0300

    new code

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,16 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'tasks#index'
+  get 'tasks', to: 'tasks#index', as: :tasks
+
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
